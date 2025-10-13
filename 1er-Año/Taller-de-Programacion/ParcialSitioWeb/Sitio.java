bb309126db4cba171124610be0dbae6fb76f7b47
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2025-10-13 12:08:49 -0300

    Update Sitio.java

--- 1er-Año/Taller-de-Programacion/ParcialSitioWeb/Sitio.java
+++ 1er-Año/Taller-de-Programacion/ParcialSitioWeb/Sitio.java
@@ -1,1 +1,45 @@
+package parcial.sitio.web;
+public abstract class Sitio {
+
+	private String nombre;
+	private String direccionWeb;
+	private int cantUsuarios;
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccionWeb() {
+		return direccionWeb;
+	}
+
+	public void setDireccionWeb(String direccionWeb) {
+		this.direccionWeb = direccionWeb;
+	}
+
+	public int getCantUsuarios() {
+		return cantUsuarios;
+	}
+
+	public void setCantUsuarios(int cantUsuarios) {
+		this.cantUsuarios = cantUsuarios;
+	}
+	public Sitio(String miNombre,String miDireccion) {
+		this.nombre = miNombre;
+		this.direccionWeb = miDireccion;
+		this.cantUsuarios = 0;
+	}
+	
+	public String toString() {
+		String aux = "";
+		aux += this.getNombre() +";" + this.getDireccionWeb() + ";" + this.getCantUsuarios() + ";" + this.cotizarSitio();
+		return aux;
+	}
+	
+	public abstract double cotizarSitio();
+}
 
