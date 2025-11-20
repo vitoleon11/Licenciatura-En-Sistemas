b09aff4f8c4b79fb523fd9aa286913fc1e728e5a
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2025-11-20 11:44:36 -0300

    Create PUNTO 3.pas

---
+++ 1er-Año/Taller-de-Programacion/IMPERATIVO/PRACTICA 4/PUNTO 3.pas
@@ -0,0 +1,29 @@
+program punto3Practica4
+type
+
+	producto = record
+		codigo:integer;
+		cantTotal:integer;
+		montoTotal:real;
+	end;
+	
+	venta = record
+		codVenta: integer;
+		codP : producto;
+		cantUnidades: integer;
+		precioU:real;
+	end;
+
+	procedure leerVenta(var v: venta);
+	var
+	begin
+	end
+
+
+
+
+var
+begin
+
+
+end.
