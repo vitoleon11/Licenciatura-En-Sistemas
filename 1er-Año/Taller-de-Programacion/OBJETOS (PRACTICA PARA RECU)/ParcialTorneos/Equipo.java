0366d7928e5bcedde7d09f335b7eca4d7fa1b576
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2025-12-03 23:18:02 -0300

    Create Equipo.java

---
+++ 1er-Año/Taller-de-Programacion/OBJETOS (PRACTICA PARA RECU)/ParcialTorneos/Equipo.java
@@ -0,0 +1,17 @@
+
+package parcialtorneos;
+
+/**
+ *
+ * @author vitol
+ */
+public class Equipo {
+        private String nombreEquipo;
+        private String origen;
+        private String nombreTecnico;
+    public Equipo(String nombreEquipo, String origen, String nombreTecnico) {
+        this.nombreEquipo = nombreEquipo;
+        this.origen = origen;
+        this.nombreTecnico = nombreTecnico;
+    }
+}
