f0fcee4ac9b6921493a11db57e6ee7c8402c0357
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2026-02-19 16:28:39 -0300

    Update VENTAS COMERCIO (9999).pas

--- 1er-Año/CADP/FINALES/VENTAS COMERCIO (9999).pas
+++ 1er-Año/CADP/FINALES/VENTAS COMERCIO (9999).pas
@@ -1,52 +1,48 @@
-
-program gork; 
+program gork;
 type
-    venta = record 
-      codVenta:integer; 
-      codProducto: integer; 
-      cantVendida:integer; 
-    end; 
-    lista =^nodo; 
-    nodo = record 
-      dato :venta; 
-      sig:lista; 
-    end; 
-    procedure leerVenta(var v:venta);
-    begin 
-      readln(v.codVenta); 
-      if (v.codVenta <> 9999) then begin 
-        readln(v.codProducto); 
-        readln(v.cantVendida); 
-        end; 
-    end; 
+    venta = record
+        codVenta:integer;
+        codProducto:integer;
+        cantVendida:integer;
+    end;
 
-    procedure insertarOrdenado(var l :lista; v:venta);
-    var
-        nue, ant, act: lista;
+    lista = ^nodo;
+    nodo = record
+        dato:venta;
+        sig:lista;
+    end;
+
+    procedure leerVenta(var v:venta);
     begin
-        new(nue);
-        nue^.dato := v;
-        nue^.sig := nil;
-    
-        if (l = nil) or (v.codProducto < l^.dato.codProducto) then begin
-            nue^.sig := l;
-            l := nue;
-        end
-        else begin
-            ant := l;
-            act := l^.sig;
-            while (act <> nil) and (act^.dato.codProducto < v.codProducto) do begin
-                ant := act;
-                act := act^.sig;
-            end;
-            ant^.sig := nue;
-            nue^.sig := act;
+        readln(v.codVenta);
+        if (v.codVenta <> 9999) then begin
+            readln(v.codProducto);
+            readln(v.cantVendida);
         end;
     end;
 
-    procedure procesarLista (var l: lista);
+    procedure insertarOrdenado(var l :lista;v:venta);
     var
-        v: venta;
+        act,ant,nue:lista;
+    begin
+        new(nue);
+        nue^.dato := v;
+        act := l;
+        ant := l;
+        while (act <> nil) and (v.codProducto > act^.dato.codProducto) do begin
+            ant := act;
+            act := act^.sig;
+        end;
+        if (act = ant) then
+            l := nue;
+        else
+            ant^.sig := nue;
+        nue^.sig := act;
+    end;
+
+    procedure cargarLista(var l :lista);
+    var
+        v:venta;
     begin
         leerVenta(v);
         while (v.codVenta <> 9999) do begin
@@ -54,29 +50,30 @@ type
             leerVenta(v);
         end;
     end;
-    
-    function informarCantidad(l:lista;codProducto:integer):integer;
+
+    procedure informarVendidos(l:lista;cod:integer);
     var
-        cant :integer;
+        aux:lista;
+        informe:false;
     begin
-        cant := 0;
-        while (l <> nil) AND (l^.dato.codProducto < codProducto) do begin
-            l := l^.sig;
+        informe := false;
+        aux:=l;
+        while (aux <> nil) and (aux^.dato.codProducto < cod) do begin
+            aux := aux^.sig;
+            while (aux <> nil) and (aux^.dato.codVenta = cod) do begin
+                cant := cant + aux^.dato.cantVendida;
+                aux := aux^.sig;
+            end;
         end;
-        while (l <> nil) AND (l^.dato.codProducto = codProducto) do begin
-            cant := cant + l^.dato.cantVendida;
-            l := l^.sig;
-        end;
-        informarCantidad := cant;
+        writeln('La cantidad vendida fue', cant);
     end;
 
 var
-    l :lista;
-    codigo:integer;
+    l:lista;
+    cod:integer;
 begin
-    l := nil;
-    procesarLista(l);
-    writeln('Ingrese un codigo de producto para saber cantidad de ventas');
-    readln(codigo);
-    writeln('La cantidad de ventas de este codigo de producto fueron ', informarCantidad(l,codigo));
+    l:= nil;
+    cargarLista(l);
+    readln(cod);
+    informarVendidos(l,cod);
 end.
