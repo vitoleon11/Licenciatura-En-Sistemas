4256335ef0d224667667824596082e1aad46d06c
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2026-04-30 18:49:55 -0300

    Update Estacion.java

--- 1er-Año/TALLER-REDICTADO/OBJETOS/Practica 4/Estacion.java
+++ 1er-Año/TALLER-REDICTADO/OBJETOS/Practica 4/Estacion.java
@@ -10,6 +10,18 @@ public class Estacion {
         this.latitud = latitud;
         this.longitud = longitud;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
     
     
 }
