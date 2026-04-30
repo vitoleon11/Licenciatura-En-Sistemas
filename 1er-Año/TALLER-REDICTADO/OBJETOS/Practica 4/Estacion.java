375c7e37a238708802f73278bda29533b26a9006
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2026-04-30 16:10:36 -0300

    Create Estacion.java

---
+++ 1er-Año/TALLER-REDICTADO/OBJETOS/Practica 4/Estacion.java
@@ -0,0 +1,15 @@
+
+package tema4;
+public class Estacion {
+    private String nombre;
+    private double latitud;
+    private double longitud;
+
+    public Estacion(String nombre, double latitud, double longitud) {
+        this.nombre = nombre;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+    
+    
+}
