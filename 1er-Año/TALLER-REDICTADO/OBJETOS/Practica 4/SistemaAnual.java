76a54626b75a1172e3fae82efb8b01cbbb435806
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2026-04-30 18:48:56 -0300

    Fix typo in toString method and improve output

--- 1er-Año/TALLER-REDICTADO/OBJETOS/Practica 4/SistemaAnual.java
+++ 1er-Año/TALLER-REDICTADO/OBJETOS/Practica 4/SistemaAnual.java
@@ -6,24 +6,18 @@ public class SistemaAnual extends Sistema {
         super(est, anioInicio, cantAnios);
     }
     
-    public String toSring() {
-        String aux = "";
+    
+    public String toString() {
+        String aux = super.getEstacion().getNombre() + " (" + super.getEstacion().getLatitud() + " - " + super.getEstacion().getLongitud() + "): \n";
         for (int i = 0; i < super.getCantAnios(); i++) {
             double suma = 0;
-
             for (int j = 0; j < 12; j++) {
-                suma += this.mTemperaturas[i][j];
+                suma += super.getTemperaturaMatriz(i,j);
             }
-
             double promedio = suma / 12.0;
-
-            sb.append("- Año ")
-              .append(anioInicio + i)
-              .append(": ")
-              .append(formatearTemp(promedio))
-              .append(" ºC;\n");
+            aux += "- Año " + (i + super.getAnioInicio()) + ": " + promedio + "; \n";
         }
-
+        return aux;
     }
     
 }
