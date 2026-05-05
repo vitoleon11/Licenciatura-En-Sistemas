6899948af3dd1e948e3073706037562122f1b87a
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2026-05-05 16:51:48 -0300

    Create Ingrediente.java

---
+++ 1er-Año/TALLER-REDICTADO/OBJETOS/PARCIALES/ParcialSanguche/Ingrediente.java
@@ -0,0 +1,14 @@
+
+public class Ingrediente {
+    private String nombre;
+    private double costo;
+    private String grupo;
+
+    public Ingrediente(String nombre, double costo, String grupo) {
+        this.nombre = nombre;
+        this.costo = costo;
+        this.grupo = grupo;
+    }
+    
+    
+}
