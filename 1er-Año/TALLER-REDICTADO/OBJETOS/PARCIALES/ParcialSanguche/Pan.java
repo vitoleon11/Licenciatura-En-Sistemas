7a5cc2daee4c5914ebcce2bebaa0b0a58f24c725
vitoleon11 <161509484+vitoleon11@users.noreply.github.com>
2026-05-05 16:51:34 -0300

    Add Pan class with properties and constructor

---
+++ 1er-Año/TALLER-REDICTADO/OBJETOS/PARCIALES/ParcialSanguche/Pan.java
@@ -0,0 +1,12 @@
+
+public class Pan {
+    private String nombre;
+    private double costo;
+    private String calidad;
+
+    public Pan(String nombre, double costo, String calidad) {
+        this.nombre = nombre;
+        this.costo = costo;
+        this.calidad = calidad;
+    }
+}
